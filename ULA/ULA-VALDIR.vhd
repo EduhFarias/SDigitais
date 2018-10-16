f20af92d45d7708093e98876bfeecfa89e483208
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-10-16 08:53:17 -0300

    Update ULA-VALDIR.vhd

--- ULA/ULA-VALDIR.vhd
+++ ULA/ULA-VALDIR.vhd
@@ -1,75 +1,72 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
-use ieee.std_logic_arith.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALLL;
 
-entity ULA is
-
-
-
-port(	A:	in std_logic_vector(32 downto 0);
-	B:	in std_logic_vector(32 downto 0);
-	con:	in std_logic_vector(5 downto 0);
-	DISPLAY_1, DISPLAY_2, DISPLAY_3 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
-	outtie:	out std_logic_vector(32 downto 0)    	
-);
-
-end ULA;
+ENTITY ULA IS
+	PORT(	
+		A	:	IN STD_LOGIC_VECTOR(32 DOWNTO 0);
+		B	:	IN STD_LOGIC_VECTOR(32 DOWNTO 0);
+		CON	:	IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+		DISPLAY_1, DISPLAY_2, DISPLAY_3 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+		D_OUT	:	OUT STD_LOGIC_VECTOR(32 DOWNTO 0)    	
+	);
+END ULA;
 
 ---------------------------------------------------
 
-architecture behv of ULA is
-signal outAdd: std_logic_vector(32 downto 0);
-signal outSub: std_logic_vector(32 downto 0);
-signal outOr: std_logic_vector(32 downto 0);
-signal outAnd: std_logic_vector(32 downto 0);
-signal Res: std_logic_vector(32 downto 0);
-begin					   
-ADDER: entity work.doSUM port map (A,B,outAdd);
-SUBBER: entity work.doSUB port map (A,B,outSub);
-ANDER: entity work.doAND port map (A,B,outAnd);
-DISPLAYER: entity work.DISPLAY port map (A,B, Res);
-ORER: entity work.doSUM port map (A,B,outOr);
-    process(A,B,con)
-    begin
-	case con is
-	    when "111100" =>  
-				Res <= outAdd; -- A+B
-	    when "111111" =>						
-	      Res <= outSub; -- B-A
-       when "001100" =>
-				Res <= outAnd; -- A and B
-	    when "011100" =>	 
-				Res <= outOr; -- A OR B
-	    when "011000" => 
-				Res <= A;  -- A
-	    when "010100" => 
-				Res <= B; -- B
-	    when "011010" =>
-				Res <= not A; -- ~ A
-	    when "101100" =>
-				Res <= not B; -- ~B
-	    when "111101" =>
-				Res <= outAdd + 1; -- 1 + A+ B
-	    when "111001" =>
-				Res <= A + 1; -- A+1
-	    when "110101" =>
-				Res <= B + 1; -- B+1
-	    when "110110" =>
-				Res <= B - 1; -- B-1
-	    when "111011" =>
-				Res <= "0000"-A;-- -A
-	    when "010000" =>
-				Res <= "0000"; -- 0
-	    when "110001" =>
-				Res <= "0001"; -- 1
-	    when "110010" =>
-				Res <= -"0001"; -- -1
-	    when others =>	 
-				Res <= "0000"; -- 0
-        end case;
-	 outtie<=Res;
-    end process;
+ARCHITECTURE BEHV OF ULA IS
+SIGNAL outAdd	: STD_LOGIC_VECTOR(32 DOWNTO 0);
+SIGNAL outSub	: STD_LOGIC_VECTOR(32 DOWNTO 0);
+SIGNAL outOr	: STD_LOGIC_VECTOR(32 DOWNTO 0);
+SIGNAL outAnd	: STD_LOGIC_VECTOR(32 DOWNTO 0);
+SIGNAL Res	: STD_LOGIC_VECTOR(32 DOWNTO 0);
 
-
-end behv;
+BEGIN					   
+ADDER	: ENTITY WORK.ADDER PORT MAP (A,B,outAdd);
+SUBBER	: ENTITY WORK.SUB PORT MAP (A,B,outSub);
+ANDER	: ENTITY WORK.LOGIC_AND PORT MAP (A,B,outAnd);
+--DISPLAYER: ENTITY WORK.DISPLAY PORT MAP (A,B, Res);
+ORER	: ENTITY WORK.LOGIC_OR PORT MAP (A,B,outOr);
+	
+PROCESS(A,B,CON)
+    BEGIN
+	CASE CON IS
+	    WHEN "111100" =>  
+		Res <= outAdd; -- A+B
+	    WHEN "111111" =>						
+	      	Res <= outSub; -- B-A
+       	    WHEN "001100" =>
+	      	Res <= outAnd; -- A and B
+	    WHEN "011100" =>	 
+		Res <= outOr; -- A OR B
+	    WHEN "011000" => 
+		Res <= A;  -- A
+	    WHEN "010100" => 
+		Res <= B; -- B
+	    WHEN "011010" =>
+		Res <= not A; -- ~ A
+	    WHEN "101100" =>
+		Res <= not B; -- ~B
+	    WHEN "111101" =>
+		Res <= outAdd + 1; -- 1 + A+ B
+	    WHEN "111001" =>
+		Res <= A + 1; -- A+1
+	    WHEN "110101" =>
+		Res <= B + 1; -- B+1
+	    WHEN "110110" =>
+		Res <= B - 1; -- B-1
+	    WHEN "111011" =>
+		Res <= "0000"-A;-- -A
+	    WHEN "010000" =>
+		Res <= "0000"; -- 0
+	    WHEN "110001" =>
+		Res <= "0001"; -- 1
+	    WHEN "110010" =>
+		Res <= -"0001"; -- -1
+	    WHEN others =>	 
+		Res <= "0000"; -- 0
+        END CASE;
+	 D_OUT <= Res;
+    END PROCESS;
+END BEHV;
